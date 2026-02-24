ef30d9e3b80a215e91b86d81602d4f766de12e89
Peter Kerfoot <peterckerfoot@gmail.com>
2026-02-24 17:35:59 -0600

    added imgur to allowed img file location

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,10 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [{ protocol: 'https', hostname: 'www.discogs.com' }],
+    remotePatterns: [
+      { protocol: 'https', hostname: 'www.discogs.com' },
+      { protocol: 'https', hostname: 'i.imgur.com' },
+    ],
   },
 }
 
