7e1df33b74f1cffa0571c911a90efff0db498e5b
Peter Kerfoot <peterckerfoot@gmail.com>
2026-03-02 15:53:16 -0600

    feat: added back to collection button

--- src/app/(protected)/collection/[copyId]/page.tsx
+++ src/app/(protected)/collection/[copyId]/page.tsx
@@ -1,6 +1,8 @@
 import { getCopy } from '@/server/queries/collection'
 import { CopyDetails } from '@/components/collection/CopyDetails'
 import { notFound } from 'next/navigation'
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 
 type Params = { copyId: string }
 type Props = { params: Promise<Params> }
@@ -12,5 +14,19 @@ export default async function CopyDetailsPage({ params }: Props) {
   const copy = await getCopy(copyId)
   if (!copy) notFound()
 
-  return <CopyDetails copy={copy} />
+  return (
+    <>
+      <CopyDetails copy={copy} />
+      <div className='flex justify-center items-center mt-8'>
+        <Link href='/collection'>
+          <Button
+            className='hover:cursor-pointer hover:text-foreground/50'
+            variant='outline'
+          >
+            Back to collection
+          </Button>
+        </Link>
+      </div>
+    </>
+  )
 }
